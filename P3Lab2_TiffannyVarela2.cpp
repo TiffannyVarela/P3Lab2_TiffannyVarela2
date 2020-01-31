d9201b9ca48d0f82b73ddab784f33dae6ece4de0
TiffannyVarela <tiffanny_varela529@unitec.edu>
2020-01-31 16:46:41 -0600

    Decimo

--- P3Lab2_TiffannyVarela2.cpp
+++ P3Lab2_TiffannyVarela2.cpp
@@ -5,7 +5,6 @@ using namespace std;
 
 int menu();
 void Ejercicio1();
-//int val_negativos(int);
 void MCD(int,int);
 
 
@@ -22,17 +21,17 @@ void arreglarArreglo(int*, int);
 
 void Ejercicio3();
 void readArray2(int*,int,int);//leer el numero de cuatro digitos
-int num_iguales(int*,int);
-void arreglarArregloMenor(int*, int);
-int devolver_numero(int*, int);
+int num_iguales(int*,int);//revisar en el arreglo sihay numeros iguales
+void arreglarArregloMenor(int*, int);//arreglar de mayor a menor
+int devolver_numero(int*, int);//devolver el numero desde el arreglo
 void readArray3(int*,int,int);//agregar ceros
 
-int main(){
+int main(){//inicio del main
 	int resp=-1;
 	int opc;
 
-	do{
-		switch(opc=menu()){
+	do{//inicio del do while
+		switch(opc=menu()){//inicio del switch
 			case 1:
 				Ejercicio1();
 				break;
@@ -42,14 +41,14 @@ int main(){
 			case 3:
 				Ejercicio3();
 				break;
-		}
+		}//fin del switch
 		cout<<"Desea volver al programa \n1.Si\n2.No"<<endl;
 		cin>>resp;
-	}while(resp!=2);
+	}while(resp!=2);//fin del do while
 	return 0;
-}
+}//fin main
 
-int menu(){
+int menu(){//inicio metodo menu
         while(true){
         cout<<"MENU"<<endl
         <<"1.- Ejercicio 1"<<endl
@@ -67,18 +66,16 @@ int menu(){
         }
  	 }//end del while
         return 0;
-}
+}//fin metodo menu
 
-void Ejercicio1(){
+void Ejercicio1(){//inicio ejercicio 1
 	int n1, n2;
 	cout<<"Ingrese numero 1: ";
 	cin>>n1;
-//	cout<<"Numero1: "<<n1<<endl;
 	while(n1<0){
 	        cout<<"Ingrese un numero positivo: ";
                 cin>>n1;
 	}
-//	cout<<"Numero1: "<<n1<<endl;
 	cout<<"Ingrese numero 2: ";
 	cin>>n2;
 	while(n2<0){
@@ -91,11 +88,9 @@ void Ejercicio1(){
 	else{
 		MCD(n1,n2);
 	}
+}//fin ejercicio 1
 
-
-}
-
-void MCD(int a, int b){
+void MCD(int a, int b){//inicio MCD
 	int r;
 	if(b==0){
 		cout<<"MCD: "<<a<<endl;
@@ -107,9 +102,9 @@ void MCD(int a, int b){
 		b=r;
 		return MCD(a,b);
 	}
-}
+}//fin MCD
 
-void Ejercicio2(){
+void Ejercicio2(){//inicio ejercicio 2
 	int tam=0;
 	cout<<"Ingrese el tamanio: ";
 	cin>>tam;
@@ -119,36 +114,36 @@ void Ejercicio2(){
 	arreglarArreglo(arreglo,tam);
 	printArray(arreglo,tam);
 	freeArray(arreglo);
-}
+}//fin ejercicio 2
 
-int* inicializar( int size){
+int* inicializar( int size){//inicio inicializar
 	int* retval = new int[size];
 	return retval;
-}
+}//fin inicializar
 
-void printArray (int* array, int size){
+void printArray (int* array, int size){//inicio print array
 	cout<<"El contenido del arreglo es: ";
 	for(int i=0; i<size;i++){
 		cout<<" ["<<array[i]<<"] ";
 	}
 	cout<<endl;
-}
+}//fin print array
 
-void freeArray(int* array){
+void freeArray(int* array){//inicio free array
 	if (array!=NULL){
 		delete[] array;
 	}
-}
+}//fin free array
 
-void readArray(int* array,int size){
+void readArray(int* array,int size){//inicio read array
 	srand(time(NULL));
 	for(int i=0; i<size;i++){
 		array[i]=rand()%100;
 	}
-}
+}//fin read array
 
 
-void arreglarArreglo(int* arreglo, int size){
+void arreglarArreglo(int* arreglo, int size){//inicio arreglar arreglo
 	int temp=-1;
 	for(int i=0; i<size; i++){
 		for(int j=0; j<size-i; j++){
@@ -159,9 +154,9 @@ void arreglarArreglo(int* arreglo, int size){
 			}
 		}
 	}
-}
+}//fin arreglar arreglo
 
-void Ejercicio3(){
+void Ejercicio3(){//inicio ejercicio 3
 	int num;
 	int* temp=inicializar(4);
 	int* temp2=inicializar(4);
@@ -194,20 +189,18 @@ void Ejercicio3(){
 	}
 	freeArray(temp);
 	freeArray(temp2);
+}//fin ejercicio 3
 
-	
-}
-
-void readArray2(int* array,int size, int num){
+void readArray2(int* array,int size, int num){//inicio read array 2
 	while(num>0){
 		array[size-1]=num%10;
 		num=num/10;
 		size--;
 	}
-}
+}//fin read array 2
 
 
-int num_iguales(int* arreglo, int size){
+int num_iguales(int* arreglo, int size){//inicio num_iguales
 	int resp = 0;
 	for(int i=0; i<size; i++){
 		for(int j=0; j<size-i; j++){
@@ -217,9 +210,9 @@ int num_iguales(int* arreglo, int size){
 		}
 	}
 	return resp;
-}
+}//fin num_iguales
 
-void arreglarArregloMenor(int* arreglo, int size){
+void arreglarArregloMenor(int* arreglo, int size){//inicio arreglar arreglo menor
         int temp=0;
         for(int i=0; i<size; i++){
                 for(int j=0; j<size-i-1; j++){
@@ -230,9 +223,9 @@ void arreglarArregloMenor(int* arreglo, int size){
                         }
                 }
         }
-}
+}//fin arreglar arreglo menor
 
-int devolver_numero(int* arreglo, int size){
+int devolver_numero(int* arreglo, int size){//inicio devolver numero
 	int num=0;
 	for(int i=0; i<size; i++){
 		switch(i){
@@ -254,5 +247,5 @@ int devolver_numero(int* arreglo, int size){
 		}
 	}
 	return num;
-}
+}//fin devolver numero
 
